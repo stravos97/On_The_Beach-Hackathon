6ca9dce53ef8bb6db899c85f328f83fd130e3340
AdamMKD <adammkd@hotmail.com>
2019-04-06 15:19:53 +0100

    yes

---
+++ Models/Database.php
@@ -0,0 +1,58 @@
+<?php
+
+class Database {
+    /**
+     * @var Database
+     */
+    protected static $_dbInstance = null;
+
+    /**
+     * @var PDO
+     */
+    protected $_dbHandle;
+
+    /**
+     * @return Database
+     */
+    public static function getInstance() {
+        /**
+         * @param $username
+         * @param $password
+         * @param $host
+         * @param $database
+         */
+        $username ='root';
+        $password = '';
+        $host = 'localhost';
+        $dbName = 'onthebeachhack';
+
+        if(self::$_dbInstance === null) { //checks if the PDO exists
+            // creates new instance if not, sending in connection info
+            self::$_dbInstance = new self($username, $password, $host, $dbName);
+        }
+
+        return self::$_dbInstance;
+    }
+
+    private function __construct($username, $password, $host, $database) {
+        try {
+            $this->_dbHandle = new PDO("mysql:host=$host;dbname=$database",  $username, $password);
+            // creates the database handle with connection info
+
+        }
+        catch (PDOException $e) { // catch any failure to connect to the database
+            echo $e->getMessage();
+        }
+    }
+
+    /**
+     * @return PDO
+     */
+    public function getdbConnection() {
+        return $this->_dbHandle; // returns the PDO handle to be used elsewhere
+    }
+
+    public function __destruct() {
+        $this->_dbHandle = null; // destroys the PDO handle when no longer needed
+    }
+}
---
+++ Models/UsersDataSet.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once ('Database.php');
+require_once ('UserData.php');
+
+class UsersDataSet
+{
+    protected $_dbHandle, $_dbInstance;
+
+    public function __construct() {
+        $this->_dbInstance = Database::getInstance();
+        $this->_dbHandle = $this->_dbInstance->getdbConnection();
+    }
+
+    public function fetchOneUser($email, $password) {
+        $sqlQuery = "SELECT * FROM users WHERE email='$email' and password = '$password' ";
+        echo $sqlQuery;
+        $statement = $this->_dbHandle->prepare($sqlQuery); // prepare a PDO statement
+        //var_dump($statement);
+        $statement->execute(); // execute the PDO statement
+
+        $user = [];
+        while ($row = $statement->fetch()) {     //fetches the next row matching the query
+            $user[] = new UserData($row);
+        }
+        return $user;
+    }
+
+}
